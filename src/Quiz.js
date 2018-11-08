c4cd8fb9afdbe610c664157aa76d6a8d55d812e3
EmilyMcGonagle <emily.mcgonagle@outlook.com>
2018-11-08 10:23:48 +0000

    module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import QuizQuestion from "./QuizQuestion.js"
+import QuizEnd from "./QuizEnd.js"
 
 //Loads json file with quiz data, stores in variable
 let quizData = require('./quiz_data.json');
@@ -10,10 +11,12 @@ class Quiz extends Component{
         this.state = { quiz_position: 1}
     }
     render(){
+        const isQuizEnd =((this.state.quiz_position -1) === quizData.quiz_questions.length);
         return(
             //Returns first elements instrutcion text in the quiz data json file 
             <div className="QuizQuestion">
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />
+                {isQuizEnd ? <QuizEnd />:
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />}
             </div>
         );
     }
